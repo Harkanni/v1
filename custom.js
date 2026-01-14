7d6c240b57236b17eaf559c7225e35178ebde8c5
harkanni <harkanzyempire001@gmail.com>
2026-01-14 15:29:42 +0100

    header animation

--- custom.js
+++ custom.js
@@ -4,8 +4,32 @@
 
   if (!header || !burger) return;
 
+  //   SIDE BAR TOGGLE
   burger.addEventListener("click", () => {
     const isOpen = header.classList.toggle("c-nav--open");
     burger.setAttribute("aria-expanded", isOpen);
+  });
+
+  //   SCROLL ANIMATION
+  let lastScrollY = window.scrollY;
+
+  window.addEventListener("scroll", () => {
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY > lastScrollY) {
+      // scrolling DOWN
+      header.classList.remove("c-nav--scroll-up");
+      header.classList.add("c-nav--scroll-down");
+    } else {
+      // scrolling UP
+      header.classList.remove("c-nav--scroll-down");
+      header.classList.add("c-nav--scroll-up");
+    }
+
+    lastScrollY = currentScrollY;
+
+    if (lastScrollY === 0) {
+      header.classList.remove("c-nav--scroll-up", "c-nav--scroll-down");
+    }
   });
 })();
